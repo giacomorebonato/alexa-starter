0555380622f3d263926a3d4daf071542bc956d02
Giacomo Rebonato <grebonato@zendesk.com>
2017-12-17 23:27:54 +0000

    Handlers in separate file

---
+++ src/handlers.ts
@@ -0,0 +1,30 @@
+import Alexa = require('alexa-sdk')
+
+export default {
+  HelloWorldIntent : function (this: Alexa.Handler<Object>) {
+    this.response.speak('Hello World!')
+    this.emit(':tell', 'Hello World!')
+  },
+  'AMAZON.HelpIntent': function (this: Alexa.Handler<Object>) {
+    this.attributes.speechOutput = this.t('HELP_MESSAGE')
+    this.attributes.repromptSpeech = this.t('HELP_REPROMPT')
+    this.emit(':ask', this.attributes.speechOutput, this.attributes.repromptSpeech)
+  },
+  'AMAZON.RepeatIntent': function (this: Alexa.Handler<Object>) {
+    this.emit(':ask', this.attributes.speechOutput, this.attributes.repromptSpeech)
+  },
+  'AMAZON.StopIntent': function (this: Alexa.Handler<Object>) {
+    this.emit('SessionEndedRequest')
+  },
+  'AMAZON.CancelIntent': function (this: Alexa.Handler<Object>) {
+    this.emit('SessionEndedRequest')
+  },
+  'SessionEndedRequest': function (this: Alexa.Handler<Object>) {
+    this.emit(':tell', this.t('STOP_MESSAGE'))
+  },
+  'Unhandled': function (this: Alexa.Handler<Object>) {
+    this.attributes.speechOutput = this.t('HELP_MESSAGE')
+    this.attributes.repromptSpeech = this.t('HELP_REPROMPT')
+    this.emit(':ask', this.attributes.speechOutput, this.attributes.repromptSpeech)
+  }
+}
